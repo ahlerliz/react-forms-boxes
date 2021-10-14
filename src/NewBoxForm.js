947f609b2db9c5bb428799543d67dbe9c807be3a
Lizzy Ahler <ahlerliz@gmail.com>
2021-10-14 14:12:28 -0500

    added files and form

---
+++ src/NewBoxForm.js
@@ -0,0 +1,60 @@
+import React, {useState} from "react";
+
+const INITIAL_STATE = {"height": "",
+                        "width": "",
+                        "color": ""};
+
+const { formData, setFormData } = useState(INITIAL_STATE);
+
+
+function NewBoxForm({addBox}){
+    
+    function handleSubmit(evt){
+        evt.preventDefault();
+        addBox(formData);
+        setFormData(INITIAL_STATE);
+    }
+
+    function handleChange(evt){
+        const {name, value} = evt.target
+        setFormData( oldData => ({
+            ...oldData,
+            [name]: value
+        }))
+    }
+    
+    return (
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="height">Height:</label>
+          <input
+            id="height"
+            name="height"
+            value={formData.height}
+            onChange={handleChange}
+          />
+
+        <label htmlFor="width">Width:</label>
+          <input
+            id="width"
+            name="width"
+            value={formData.width}
+            onChange={handleChange}
+          />
+    
+          <label htmlFor="color">Background color:</label>
+          <input
+            type="number"
+            id="color"
+            name="color"
+            value={formData.color}
+            onChange={handleChange}
+          />
+    
+          <button>Add a new box!</button>
+        </form>
+      );
+
+}
+
+
+
